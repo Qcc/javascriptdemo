b864b5a638f9ab262c379fc9bf94864e71ff224d
kevin <kevin.e@qq.com>
2017-01-04 17:46:33 +0800

    kevin

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function loacXML() {
   xmlhttp.onreadystatechange
 }
 loacXML();
+
+function Foo(name,color) {
+  this.name = name;
+  this.color = color;
+  this.whatYouAre =function(){
+    return "I\'am "+this.name +" " + this.color;
+  }
+}
+
+;Foo.prototype = {
+  sex:"man",
+  say:function () {
+    return "I\'am"+this.name+" "+this.color+" and "+this.sex;
+  },
+  sayhello:function(){return this.name+" hello!"}
+}
+
+;Foo.prototype = {
+  sex:"man",
+  say:function () {
+    return "I\'am"+this.name+" "+this.color+" and "+this.sex;
+  },
+  sayhello:function(){return this.name+" hello!"}
+}
